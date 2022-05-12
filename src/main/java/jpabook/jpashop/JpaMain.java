fc462514170d99ff2cf1b2dc0cdbe76ead05765b
minMin <goals1005@naver.com>
2022-05-12 13:20:02 +0900

    다양한 연관관계

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,6 +19,16 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Order order = new Order();
+            em.persist(order);
+//            order.addOrderItem(new OrderItem());
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
             tx.commit();//쓰기지연 SQL 저장소에 SQL문을 DB에 보냄
         } catch (Exception e) {
             tx.rollback();
